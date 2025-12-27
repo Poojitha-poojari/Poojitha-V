d0ee99f8811a1b8c745d8cd8b6ee0e5acd2c2566
Poojitha-poojari <143061513+Poojitha-poojari@users.noreply.github.com>
2025-12-27 22:52:06 +0530

    Update Problem-3.java

--- Problem-3.java
+++ Problem-3.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class Problem3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i += 2) {
+            if (i == a && a % 2 == 0)
+                break;
+
+            System.out.print(i);
+            if (i + 2 < a || (i + 2 == a && a % 2 != 0))
+                System.out.print(", ");
+        }
+
+	}
+
+}
