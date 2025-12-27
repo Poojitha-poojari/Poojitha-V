b53360fc88df4e7c91c20e2e4c1da659ae00655b
Poojitha-poojari <143061513+Poojitha-poojari@users.noreply.github.com>
2025-12-27 23:00:15 +0530

    Update Problem-4.java

--- Problem-4.java
+++ Problem-4.java
@@ -1,1 +1,23 @@
+public class Problem4 {
+    public static void main(String[] args) {
 
+        int[] nums = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+
+        int[] count = new int[10]; 
+
+        for (int i = 1; i <= 9; i++) {
+            for (int n : nums) {
+                if (n % i == 0) {
+                    count[i]++;
+                }
+            }
+        }
+
+        System.out.print("{");
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i + ": " + count[i]);
+            if (i < 9) System.out.print(", ");
+        }
+        System.out.print("}");
+    }
+}
